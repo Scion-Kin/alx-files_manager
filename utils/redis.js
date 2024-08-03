849ea6a79632b0224fc26b1e116b7a83989b6e9b
Scion-Kin <dinturner17@gmail.com>
2024-08-03 21:33:53 +0200

    Task 0 done

---
+++ utils/redis.js
@@ -0,0 +1,29 @@
+import { createClient } from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.connected = true;
+    this.client.on('error', (error) => { console.log(error.message); });
+  }
+
+  isAlive() {
+    return this.connected;
+  }
+
+  async get(key) {
+    return promisify(this.client.get).bind(this.client)(key);
+  }
+
+  async set(key, duration, value) {
+    await promisify(this.client.setex).bind(this.client)(key, duration, value);
+  }
+
+  async del(key) {
+    await promisify(this.client.DEL).bind(this.client)(key);
+  }
+}
+
+export const redisClient = new RedisClient();
+export default redisClient;
