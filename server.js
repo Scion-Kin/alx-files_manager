a035bf4f174e702fe25ee6997ca71d9fe581d4db
Scion-Kin <dinturner17@gmail.com>
2024-08-07 18:17:08 +0200

    Task 4 done.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const routes = require('./routes/index');
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 
 routes(app);
 app.listen(process.env.PORT || 5000);
