a7d08856dc67415bdc9e821e986804da0d728ae0
Scion-Kin <dinturner17@gmail.com>
2024-08-08 09:03:00 +0200

    Task 4 done.

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -4,17 +4,21 @@ const { redisClient } = require('../utils/redis');
 const dbClient = require('../utils/db');
 
 export async function getConnect(req, res) {
-  const auth = req.headers.authorization.split(' ')[1];
-  const [email, password] = Buffer.from(auth, 'base64').toString('utf-8').split(':');
-  const token = v4();
-  const collection = await dbClient.getClient('users');
-  const user = await collection.findOne({ email, password: sha1(password) });
+  try {
+    const auth = req.headers.authorization.split(' ')[1];
+    const [email, password] = Buffer.from(auth, 'base64').toString('utf-8').split(':');
+    const token = v4();
+    const collection = await dbClient.getClient('users');
+    const user = await collection.findOne({ email, password: sha1(password) });
 
-  if (!user) {
+    if (!user) {
+      res.status(401).json({ error: 'Unauthorized' });
+    } else {
+      await redisClient.set(`auth_${token}`, user._id.toString(), 24 * 60 * 60);
+      res.status(200).json({ token });
+    }
+  } catch (error) {
     res.status(401).json({ error: 'Unauthorized' });
-  } else {
-    await redisClient.set(`auth_${token}`, user._id.toString(), 24 * 60 * 60);
-    res.status(200).json({ token });
   }
 }
 
